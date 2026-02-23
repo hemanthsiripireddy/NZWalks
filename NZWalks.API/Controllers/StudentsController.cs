f69be2461a2e676c5793e0d28bdee6be1edba970
Siripireddy, Hemanth <Hemanth.Siripireddy@verint.com>
2026-02-23 16:00:28 +0530

    Intial Practice code for .net webapi

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            var students = new List<string>
+            {
+                "John Doe",
+                "Jane Smith",
+                "Michael Johnson"
+            };
+            return Ok(students);
+        }
+    }
+}
---
+++ NZWalks.API/Program.cs
@@ -0,0 +1,36 @@
+using NZWalks.API.Data;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("NZWalksConnectionString");
+
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 21)));
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
